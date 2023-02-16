6dd01dc7f879d890785ac4d12d325423f38c2c34
alphonsi <alphonsio@yahoo.com>
2023-02-16 12:18:35 -0800

    added ./size32

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -  print size of various types of variables
+ *
+ * variables: char, int, long int, long long int and float
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+	
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
